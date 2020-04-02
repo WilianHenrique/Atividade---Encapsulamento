db9bebae7357125d3258c35f2fdb79fa93aaa45a
Wilian Henrique <wilian.oliveira@alunos.unis.edu.br>
2020-04-02 13:56:05 -0300

    Add project files.

---
+++ ProgCarimbo/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+
+       / instância da classe carimbo /
+        Carimbo C = new Carimbo("Aprovado");
+        C.carregar("Preto", 2);
+        C.usar();
+
+        C.trocarCor("Azul");
+        C.usar();
+
+
+        C.carregar("Preto", 2);
+        C.trocarTexto("Recuperação");
+        C.usar();
+    }
+}
